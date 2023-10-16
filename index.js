fb59327af66e36510339cde618c928d67585e231
Isaac <govegeku00@gmail.com>
2023-10-16 11:55:59 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+const names = ['Charlie', 'Samip', 'Ali'];
+  const eventName = 'birthday';
+  const result = writeCards(names, eventName);
+  console.log(result);
+  
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+      debugger;
+    }
+    return messages;
+  }
+  
+function countDown(startingNumber) {
+  if (startingNumber < 0) {
+    console.log("Please provide a positive integer.");
+    return;
+  }
+
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+countDown(10); // Call the function with any positive integer
+
+
+  
